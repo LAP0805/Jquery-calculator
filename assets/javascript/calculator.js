85f8aa5848ae79ea501f48f8d943c7f543b9ea7e
Janxxy <lesley.pond@gmail.com>
2020-03-29 10:37:41 -0400

    formatting code

--- assets/javascript/calculator.js
+++ assets/javascript/calculator.js
@@ -1,124 +1,114 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-var occupied="";
-var x = "";
-var y = "";
-var operator="";
-var decimalOcc="";
+  var occupied = "";
+  var x = "";
+  var y = "";
+  var operator = "";
+  var decimalOcc = "";
 
 
-function numberClick(){
-$(".number").on("click", function(){
-var number = this.value;
-   if (!occupied){
-     $("#display").append(number);
-     x = $("#display").text();
-     console.log("x="+ x);
-  
-   }
-   else if (occupied === "occupied"){
-   
-    $("#display").append(number);
-     y =  $("#display").text();
-     console.log("y=" +y);
-     
-     
-     
-   }
-});
-}
-numberClick();
-function operatorClick(){
-$(".operator").on("click",function(){
-  if (x){
-    if (!occupied){
-    var operatorVal=this.value;
-    $("#display").text("");
-    operator= operatorVal;
-    occupied = "occupied"
-    decimalOcc = "";
-    } 
-    else {
-        return;
-    }
+  function numberClick() {
+    $(".number").on("click", function () {
+      var number = this.value;
+      if (!occupied) {
+        $("#display").append(number);
+        x = $("#display").text();
+        console.log("x=" + x);
+
+      } else if (occupied === "occupied") {
+
+        $("#display").append(number);
+        y = $("#display").text();
+        console.log("y=" + y);
+
+
+
+      }
+    });
   }
-});
-  
-}
-operatorClick();
-
-
-$("#decimal").on("click", function(){
-  
-  if(!decimalOcc){
-  $("#display").append(".");
-  decimalOcc = "occupied";
-  }
-  else{
-    return;
-  }
-
-});
-
-
-
-$("#clear").on("click",function(){
-  
-  $("#display").text("");
-  x="";
-  y="";
-  operator="";
-  occupied="";
   numberClick();
-  decimalOcc = "";
-});
 
-$("#equals").on("click",function(){
-  var a = parseFloat(x);
-  var b = parseFloat(y);
-  console.log("x=" +x);
-  console.log("y=" +y);
- if (operator === "+"){
-   var total = a + b;
-   var newtotal=( Math.floor(total * 100) / 100 );
-    $("#display").text(newtotal);
-   $(".number").off("click")
- }
-  else if (operator === "-"){
-    var total = a - b;
-    var newtotal=( Math.floor(total * 100) / 100 );
-    $("#display").text(newtotal);
-    $(".number").off("click")
+  function operatorClick() {
+    $(".operator").on("click", function () {
+      if (x) {
+        if (!occupied) {
+          var operatorVal = this.value;
+          $("#display").text("");
+          operator = operatorVal;
+          occupied = "occupied"
+          decimalOcc = "";
+        } else {
+          return;
+        }
+      }
+    });
+
   }
-  else if (operator === "*"){
-    var total = a * b;
-    var newtotal=( Math.floor(total * 100) / 100 );
-    $("#display").text(newtotal);
-    $(".number").off("click")
-  }
-  else if (operator === "/"){
-    var total = (a/b);
-    var newtotal=( Math.floor(total * 100) / 100 );
-    $("#display").text(newtotal);
-    $(".number").off("click")
-  }
-  else if (operator === "^"){
-    var total = a ** b;
-    var newtotal=( Math.floor(total * 100) / 100 );
-    $("#display").text(newtotal);
-    $(".number").off("click")
-  }
-  else if (!operator ){
-    $(".number").off("click")
-    return  $("#display").text("Error");
-    
-  }
+  operatorClick();
 
-  
- });
+
+  $("#decimal").on("click", function () {
+
+    if (!decimalOcc) {
+      $("#display").append(".");
+      decimalOcc = "occupied";
+    } else {
+      return;
+    }
+
+  });
 
 
 
+  $("#clear").on("click", function () {
+
+    $("#display").text("");
+    x = "";
+    y = "";
+    operator = "";
+    occupied = "";
+    numberClick();
+    decimalOcc = "";
+  });
+
+  $("#equals").on("click", function () {
+    var a = parseFloat(x);
+    var b = parseFloat(y);
+    console.log("x=" + x);
+    console.log("y=" + y);
+    if (operator === "+") {
+      var total = a + b;
+      var newtotal = (Math.floor(total * 100) / 100);
+      $("#display").text(newtotal);
+      $(".number").off("click")
+    } else if (operator === "-") {
+      var total = a - b;
+      var newtotal = (Math.floor(total * 100) / 100);
+      $("#display").text(newtotal);
+      $(".number").off("click")
+    } else if (operator === "*") {
+      var total = a * b;
+      var newtotal = (Math.floor(total * 100) / 100);
+      $("#display").text(newtotal);
+      $(".number").off("click")
+    } else if (operator === "/") {
+      var total = (a / b);
+      var newtotal = (Math.floor(total * 100) / 100);
+      $("#display").text(newtotal);
+      $(".number").off("click")
+    } else if (operator === "^") {
+      var total = a ** b;
+      var newtotal = (Math.floor(total * 100) / 100);
+      $("#display").text(newtotal);
+      $(".number").off("click")
+    } else if (!operator) {
+      $(".number").off("click")
+      return $("#display").text("Error");
+
+    }
+
+
+  });
 
 
 
@@ -127,5 +117,8 @@ $("#equals").on("click",function(){
 
 
 
-//ready closing tag    
+
+
+
+  //ready closing tag    
 });
